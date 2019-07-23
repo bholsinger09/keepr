876d6d27f108fee2a8548376e5ae15c68fb10919
ben <bholsinger@gmail.com>
2019-07-23 15:26:50 -0600

    building out home view

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="home">
-    <h1>Welcome Home {{user.username}}</h1>
-    <button v-if="user.id" @click="logout">logout</button>
-    <router-link v-else :to="{name: 'login'}">Login</router-link>
+    <header class="welcome">
+      <h1>Welcome Home {{user.username}}</h1>
+      <button v-if="user.id" @click="logout">logout</button>
+      <router-link v-else :to="{name: 'login'}">Login</router-link>
+    </header>
+    <main>
+      <keep></keep>
+
+    </main>
+
+
+
   </div>
 </template>
 
 <script>
-export default {
-  name: "home",
-  computed: {
-    user() {
-      return this.$store.state.user;
+  export default {
+    name: "home",
+    computed: {
+      user() {
+        return this.$store.state.user;
+      }
+    },
+
+    methods: {
+      logout() {
+        this.$store.dispatch("logout");
+      }
     }
-  },
-  methods: {
-    logout() {
-      this.$store.dispatch("logout");
-    }
-  }
-};
+  };
 </script>
